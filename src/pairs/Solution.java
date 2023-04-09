7bf3922a8547a4e2966c772e0c072e4a09ce31a1
hoang92hh <hoang92honghoa@gmail.com>
2023-04-09 08:37:41 +0700

    pairs-newString-replace

--- src/pairs/Solution.java
+++ src/pairs/Solution.java
@@ -14,7 +14,7 @@ public class Solution {
     private static int R;
 
     private static int[][] arr;
-    private static int[] list;
+    private static String list;
     private static int minPoint;
     private static int minOfMaxPoint;
     private static int sumOfCase;
@@ -28,7 +28,7 @@ public class Solution {
         Scanner sc =  new Scanner(new FileInputStream("pairs2.txt"));
 //        Scanner sc =  new Scanner(new FileInputStream("pairs.txt"));
 //        Scanner sc = new Scanner(System.in);
-         TC = sc.nextInt();
+        TC = sc.nextInt();
 
         for (int tc = 1; tc <= TC; tc++) {
             R = sc.nextInt();
@@ -43,9 +43,9 @@ public class Solution {
 
             }
 
-            list = new int[R];
+            list = "";
             for (int j = 0; j < R; j++) {
-                list[j] = j + 1;
+                list +=(j+1);
             }
 
             getMinPoint();
@@ -63,10 +63,10 @@ public class Solution {
     private static void getMaxPoint() {
         minOfMaxPoint = 0;
         sumOfCase = R*(R+1)/2;
-        handleMaxDFS(new int[R], 0, 0);
+        handleMaxDFS(list, 0, 0);
     }
 
-    private static void handleMaxDFS(int[] visited, int minOfMaxCase, int row) {
+    private static void handleMaxDFS(String personlist, int minOfMaxCase, int row) {
         if(minOfMaxCase>=minOfMaxPoint && minOfMaxPoint !=0){
             return;
         }
@@ -77,24 +77,49 @@ public class Solution {
             }
             return;
         }
-        for (int i = R-1; i >=0 ; i--) {
-            int personB = arr[row][i];
-            if(visited[personB-1]==0){
-                visited[personB-1]=1;
-                handleMaxDFS(visited, minOfMaxCase + (sumOfCase-i-1),  row+1);
-                visited[personB-1]=0;
+        for (int i = 0; i <personlist.length(); i++) {
+            int personB = personlist.charAt(i);
+            int pointB = getPointOfPerson(arr,row,personB);
+            String newString = personlist.replace(String.valueOf(personB), "");
+            handleMaxDFS(newString, minOfMaxCase + (sumOfCase-pointB),  row+1);
+
+        }
+    }
+
+    private static int[] deletePerson(int[] personlist, int personB) {
+        int newLength = personlist.length- 1;
+        int[] newArr = new int[newLength];
+        boolean next = false;
+        for (int i = 0; i < newLength; i++) {
+            if (personlist[i] == personB) {
+                next = true;
+                newArr[i] = personlist[i+1];
+            } else if (next) {
+                newArr[i] = personlist[i+1];
+            } else {
+                newArr[i] = personlist[i];
             }
         }
+        return newArr;
+
+    }
+
+    private static int getPointOfPerson(int[][] arr, int row, int person) {
+        for (int i = 0; i <R ; i++) {
+            if(arr[row][i] == person) return i+1;
+        }
+
+        return 0;
     }
 
     private static void getMinPoint() {
         minPoint = 0;
-        handleMinDFS(new int[R], 0, 0);
+        handleMinDFS(list, 0, 0);
 
 
     }
 
-    private static void handleMinDFS(int[] visited, int minOfCase, int row) {
+    private static void handleMinDFS(String personList, int minOfCase, int row) {
         //condition to backtrack
         if(minOfCase>=minPoint && minPoint !=0){
             return;
@@ -106,14 +131,11 @@ public class Solution {
             }
             return;
         }
-        for (int i = 0; i <R ; i++) {
-            int personB = arr[row][i];
-            if(visited[personB-1]==0){
-                visited[personB-1]=1;
-                handleMinDFS(visited, minOfCase+ i+1,  row+1);
-                visited[personB-1]=0;
-            }
-
+        for (int i = 0; i <personList.length(); i++) {
+            int personB = personList.charAt(i);
+            int pointB = getPointOfPerson(arr,row,personB);
+            String  newString = personList.replace(String.valueOf(personB), "");
+            handleMinDFS(newString,  minOfCase  + pointB,  row+1);
         }
 
     }
