9eae1c0c4d07e92a3863bcd4c442fa52d5e6c52d
hoang92hh <hoang92honghoa@gmail.com>
2023-04-09 10:52:34 +0700

    first commit in pair_stackLibrary

--- src/pairs/Solution.java
+++ src/pairs/Solution.java
@@ -1,8 +1,6 @@
 package pairs;
 
 
-import sun.awt.EventQueueItem;
-
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
@@ -21,12 +19,9 @@ public class Solution {
     private static int sumOfCase;
 
 
-
-
-
     public static void main(String[] args) throws FileNotFoundException {
-        System.out.println("---time start --- "+ LocalTime.now());
-        Scanner sc =  new Scanner(new FileInputStream("pairs2.txt"));
+        System.out.println("---time start --- " + LocalTime.now());
+        Scanner sc = new Scanner(new FileInputStream("pairs2.txt"));
 //        Scanner sc =  new Scanner(new FileInputStream("pairs.txt"));
         TC = sc.nextInt();
 
@@ -45,64 +40,131 @@ public class Solution {
             arrExtend = new int[R][R];
             for (int j = 0; j < R; j++) {
                 for (int k = 0; k < R; k++) {
-                    arrExtend[j][arr[j][k]-1] =k+1;
+                    arrExtend[j][arr[j][k] - 1] = k + 1;
                 }
 
             }
 
             list = new int[R];
             for (int j = 0; j < R; j++) {
-                list[j] =(j+1);
+                list[j] = (j + 1);
             }
 
             getMinPoint();
             getMaxPoint();
-            System.out.println("#" + tc +" "+minPoint + " " + (R*sumOfCase-minOfMaxPoint) );
-
+            System.out.println("#" + tc + " " + minPoint + " " + (R * sumOfCase - minOfMaxPoint));
 
 
         }
-        System.out.println("---time end --- "+ LocalTime.now());
+        System.out.println("---time end --- " + LocalTime.now());
 
 
     }
+
+    private static void getMinPoint() {
+        minPoint = 0;
+
+        Stack<Integer> minStack = new Stack<>();
+        Stack<int[]> neiborStack = new Stack<>();
+        Stack<Integer> rowStack = new Stack<>();
+
+        for (int i = 0; i < list.length; i++) {
+            minStack.push(arrExtend[0][i]);
+            neiborStack.push(deletePerson(list, list[i]));
+            rowStack.push(1);
+            handleMinDFS(minStack, neiborStack, rowStack);
+
+        }
+
+
+    }
+
 
     private static void getMaxPoint() {
+        Stack<Integer> minOfMaxStack = new Stack<>();
+        Stack<int[]> neiborStack = new Stack<>();
+        Stack<Integer> rowStack = new Stack<>();
+
+
         minOfMaxPoint = 0;
         sumOfCase = R*(R+1)/2;
-        handleMaxDFS(list, 0, 0);
+        for (int i = 0; i <list.length ; i++) {
+            minOfMaxStack.push(sumOfCase-arrExtend[0][list[i]-1]);
+            neiborStack.push(deletePerson(list, list[i]));
+            rowStack.push(1);
+            handleMaxDFS(minOfMaxStack,neiborStack,rowStack);
+        }
+
     }
 
-    private static void handleMaxDFS(int[] personlist, int minOfMaxCase, int row) {
-        if(minOfMaxCase>=minOfMaxPoint && minOfMaxPoint !=0){
-            return;
-        }
-        //condition to set MinPoint again
-        if(row==R){
-            if(minOfMaxCase<minOfMaxPoint || minOfMaxPoint==0){
-                minOfMaxPoint = minOfMaxCase;
+    private static void handleMinDFS(Stack<Integer> minStack, Stack<int[]> neiborStack, Stack<Integer> rowStack) {
+        while(!minStack.isEmpty()){
+            int minOfCase = minStack.pop();
+            int[] nextNeibor = neiborStack.pop();
+            int row = rowStack.pop();
+
+            //condition to backtrack
+            if(minOfCase>=minPoint && minPoint !=0){
+                continue;
             }
-            return;
+            //
+            if(row==R){
+                if(minOfCase<minPoint || minPoint==0){
+                    minPoint = minOfCase;
+                }
+                continue;
+            }
+
+            for (int i = 0; i <nextNeibor.length ; i++) {
+                int pointB = arrExtend[row][nextNeibor[i]-1];
+
+                minStack.push(minOfCase+ pointB);
+                neiborStack.push(deletePerson(nextNeibor, nextNeibor[i]));
+                rowStack.push(row+1);
+            }
         }
-        for (int i = 0; i <personlist.length; i++) {
-            int personB = personlist[i];
-            int pointB = arrExtend[row][personB-1];
-            int[] newString = deletePerson(personlist, personB);
-            handleMaxDFS(newString, minOfMaxCase + (sumOfCase-pointB),  row+1);
+    }
+
+    private static void handleMaxDFS(Stack<Integer> minOfMaxStack, Stack<int[]> neiborStack, Stack<Integer> rowStack) {
+        while (!minOfMaxStack.isEmpty()) {
+            //get element trong stack
+            int minOfMaxCase = minOfMaxStack.pop();
+            int[] nextNeibor = neiborStack.pop();
+            int row = rowStack.pop();
+
+            //condition to backtrack
+            if(minOfMaxCase>=minOfMaxPoint && minOfMaxPoint !=0){
+                continue;
+            }
+            //condition to set MinOfMaxPoint
+            if(row==R){
+                if(minOfMaxCase<minOfMaxPoint || minOfMaxPoint==0){
+                    minOfMaxPoint = minOfMaxCase;
+                }
+                continue;
+            }
+
+            for (int i = 0; i <nextNeibor.length ; i++) {
+                int pointB = arrExtend[row][nextNeibor[i]-1];
+
+                minOfMaxStack.push(minOfMaxCase+ sumOfCase-pointB);
+                neiborStack.push(deletePerson(nextNeibor, nextNeibor[i]));
+                rowStack.push(row+1);
+            }
 
         }
     }
 
     private static int[] deletePerson(int[] personlist, int personB) {
-        int newLength = personlist.length- 1;
+        int newLength = personlist.length - 1;
         int[] newArr = new int[newLength];
         boolean next = false;
         for (int i = 0; i < newLength; i++) {
             if (personlist[i] == personB) {
                 next = true;
-                newArr[i] = personlist[i+1];
+                newArr[i] = personlist[i + 1];
             } else if (next) {
-                newArr[i] = personlist[i+1];
+                newArr[i] = personlist[i + 1];
             } else {
                 newArr[i] = personlist[i];
             }
@@ -111,41 +173,6 @@ public class Solution {
 
     }
 
-    private static int getPointOfPerson(int[][] arr, int row, int person) {
-        for (int i = 0; i <R ; i++) {
-            if(arr[row][i] == person) return i+1;
-        }
-
-        return 0;
-    }
-
-    private static void getMinPoint() {
-        minPoint = 0;
-        handleMinDFS(list, 0, 0);
-
-
-    }
-
-    private static void handleMinDFS(int[] personList, int minOfCase, int row) {
-        //condition to backtrack
-        if(minOfCase>=minPoint && minPoint !=0){
-            return;
-        }
-        //condition to set MinPoint again
-        if(row==R){
-            if(minOfCase<minPoint || minPoint==0){
-                minPoint = minOfCase;
-            }
-            return;
-        }
-        for (int i = 0; i <personList.length; i++) {
-            int personB = personList[i];
-            int pointB = arrExtend[row][personB-1];
-            int[] newString =deletePerson(personList, personB);
-            handleMinDFS(newString,  minOfCase  + pointB,  row+1);
-        }
-
-    }
 
 
 
